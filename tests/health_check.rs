8ad9c2203d107868727a3bbd0c629faf776a872e
Michael Cuffaro <michael@cuffaro.com>
2023-04-09 09:23:10 +0200

    WIP: subscriptions endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -28,11 +28,9 @@ async fn greet_works() {
         .expect("failed to execute request.");
 
     assert!(response.status().is_success());
-    let body = match response.text().await {
-        Ok(body) => body,
-        _ => panic!(),
-    };
-    assert_eq!("Hello World", body);
+    let body = response.text().await;
+    assert!(body.is_ok());
+    assert_eq!("Hello World", body.unwrap());
 }
 
 #[tokio::test]
@@ -48,11 +46,55 @@ async fn custom_greet_works() {
         .expect("failed to execute request.");
 
     assert!(response.status().is_success());
-    let body = match response.text().await {
-        Ok(body) => body,
-        _ => panic!(),
-    };
-    assert_eq!("Hello maiku", body);
+    let body = response.text().await;
+    assert!(body.is_ok());
+    assert_eq!("Hello maiku", body.unwrap());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {
